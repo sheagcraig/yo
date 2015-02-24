21e6d6d6e20113ea494a764c8568aad46bb3c071
Shea Craig <shea.craig@da.org>
2015-02-24 15:50:39 -0500

    Initial commit. Copied from testing files.

--- yo/AppDelegate.swift
+++ yo/AppDelegate.swift
@@ -14,7 +14,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // Create a user notification object and set it's properties.
+        let notification = NSUserNotification()
+        notification.title = "Taco Emergency"
+
+        for argument in Process.arguments {
+            println(argument)
+        }
+        
+        notification.subtitle = "There are not enough tacos to sustain life."
+        notification.informativeText = "In girum imus nocte et consumimur igni."
+        notification.contentImage = NSImage(contentsOfFile: "/Users/scraig/Desktop/Lo-Pan.jpg")
+        notification.hasActionButton = true
+        notification.actionButtonTitle = "Buy Tacos"
+        
+        // Set delivery time in the future and configure notification.
+        let deliveryTime = NSDate().dateByAddingTimeInterval(5)
+        notification.deliveryDate = deliveryTime
+        
+        // Schedule notification with the notification center.
+        NSUserNotificationCenter.defaultUserNotificationCenter().scheduleNotification(notification)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {

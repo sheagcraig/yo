fd97767346c7ec154a35d3b769caf9f23211c2e1
Shea Craig <shea.craig@da.org>
2015-02-24 15:36:01 -0500

    Initial Commit

---
+++ yo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  yo
+//
+//  Created by Shea Craig on 2/24/15.
+//  Copyright (c) 2015 Soap Zombie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ yo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  yo
+//
+//  Created by Shea Craig on 2/24/15.
+//  Copyright (c) 2015 Soap Zombie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

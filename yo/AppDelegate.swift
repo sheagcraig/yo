8623b44459ef933c13dab044e556400f20a5148a
Shea Craig <shea.craig@da.org>
2015-02-25 11:50:18 -0500

    Handle commandline arguments. Make optional action buttoning.

--- yo/AppDelegate.swift
+++ yo/AppDelegate.swift
@@ -19,20 +19,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         let cli = CommandLine()
         
         let title = StringOption(shortFlag: "t", longFlag: "title", required: true, helpMessage: "Title for notification")
+        let subtitle = StringOption(shortFlag: "s", longFlag: "subtitle", required: false, helpMessage: "Subtitle for notification")
+        let informative_text = StringOption(shortFlag: "i", longFlag: "itext", required: false, helpMessage: "Informative text.")
+        let action_btn_text = StringOption(shortFlag: "b", longFlag: "btext", required: false, helpMessage: "Action button text.")
         
         cli.addOptions(title)
         let (success, error) = cli.parse()
+        
         // Create a user notification object and set it's properties.
         let notification = NSUserNotification()
 
         // For now, cmdline arg processing is VERY unfinished. Expects
         // arguments in order: title, subtitle, informativeText, actionButtonTitle
         notification.title = title.value
-        notification.subtitle = "Test"
-        notification.informativeText = "Test"
+        notification.subtitle = subtitle.value?
+        notification.informativeText = informative_text.value?
+
+        if let btn_text = action_btn_text.value? {
+            notification.hasActionButton = true
+            notification.actionButtonTitle = btn_text
+        }
+        else {
+            notification.hasActionButton = false
+        }
+
         //notification.contentImage = NSImage(contentsOfFile: "/Users/scraig/Desktop/Lo-Pan.jpg")
-        notification.hasActionButton = true
-        notification.actionButtonTitle = "Test"
+
         
         // Set delivery time in the future and configure notification.
         let deliveryTime = NSDate().dateByAddingTimeInterval(5)

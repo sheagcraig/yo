604bded012f215301518eab792c61af250aa305a
Shea Craig <shea.craig@da.org>
2015-02-25 15:43:58 -0500

    Add usage handler, rename long flags. Add README.

---
+++ README.md
@@ -0,0 +1,18 @@
+yo
+========
+
+```
+Usage: /Applications/yo.app/Contents/MacOS/yo [options]
+  -t, --title:   
+      Title for notification
+  -s, --subtitle:
+      Subtitle for notification
+  -i, --itext:   
+      Informative text.
+  -b, --btext:   
+      Action button text.
+  -o, --obtext:  
+      Alternate label for cancel button text.
+  -a, --action:  
+      Application to open if user selects the action button. Provide the full path as the argument.
+```
--- yo/AppDelegate.swift
+++ yo/AppDelegate.swift
@@ -20,13 +20,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         
         let title = StringOption(shortFlag: "t", longFlag: "title", required: true, helpMessage: "Title for notification")
         let subtitle = StringOption(shortFlag: "s", longFlag: "subtitle", required: false, helpMessage: "Subtitle for notification")
-        let informative_text = StringOption(shortFlag: "i", longFlag: "itext", required: false, helpMessage: "Informative text.")
-        let action_btn_text = StringOption(shortFlag: "b", longFlag: "btext", required: false, helpMessage: "Action button text.")
-        let other_btn_text = StringOption(shortFlag: "o", longFlag: "obtext", required: false, helpMessage: "Alternate label for cancel button text.")
-        let action = StringOption(shortFlag: "a", longFlag: "action", required: false, helpMessage: "Application to open if user selects the action button. Provide the full path as the argument.")
+        let informative_text = StringOption(shortFlag: "i", longFlag: "info", required: false, helpMessage: "Informative text.")
+        let action_btn_text = StringOption(shortFlag: "b", longFlag: "action_btn", required: false, helpMessage: "Action button text.")
+        let other_btn_text = StringOption(shortFlag: "o", longFlag: "other_btn", required: false, helpMessage: "Alternate label for cancel button text.")
+        let action = StringOption(shortFlag: "a", longFlag: "action_path", required: false, helpMessage: "Application to open if user selects the action button. Provide the full path as the argument.")
         
         cli.addOptions(title, subtitle, informative_text, action_btn_text, other_btn_text, action)
         let (success, error) = cli.parse()
+        if success != false {
+            println(success)
+            println(error!)
+            println(other_btn_text.value?)
+            cli.printUsage()
+            exit(EX_USAGE)
+        }
         
         // Create a user notification object and set it's properties.
         let notification = NSUserNotification()
